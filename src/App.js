51cb176b798bb03f14e6e7e89007bad6c2524855
Aaron Wilson <aaronwilson1234@outlook.ie>
2021-03-07 18:02:32 +0000

    creating formInput, signIn and customButton component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
+
 import { Switch, Route } from "react-router-dom";
+
 import HomePage from '../src/pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx";
+
 import Header from "./components/header/header.component";
 
 
@@ -20,6 +24,7 @@ function App() {
       <Route exact path="/" component={HomePage} />
       <Route exact path="/hats" component={HatsPage} />
       <Route exact path="/shop" component={ShopPage} />
+      <Route exact to="/signin" component={SignInAndSignUpPage} />
     </Switch>
     </div>
   );
